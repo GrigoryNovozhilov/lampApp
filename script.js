ec72b4c9fe40ef3e9a1737f9b153829f0437da38
Grigory Novozhilov <grigorynovozhilov8@gmail.com>
2023-09-19 22:02:06 +0300

    first init

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict' // строгий режим — нужен, чтобы подключить все возможности нового JS
+console.log('script was loaded...');    
+
+let nameLamp = 'MyLamp';
+console.log(nameLamp);
+
+let lampHeight, lampColor, lampLightColor;
+lampHeight = 50;
+lampColor = 'black';
+lampLightColor = 'warmwhite';
+
+lampColor = 'blue';
